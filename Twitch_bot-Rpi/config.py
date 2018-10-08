333d6e2f405de509c0e7196fe8aa384a3c04a0dc
Manodhayan <manodhayan@hotmail.com>
2018-10-08 13:28:36 +0100

    readme file modified

--- Twitch_bot-Rpi/config.py
+++ Twitch_bot-Rpi/config.py
@@ -1,11 +1,10 @@
 HOST = "irc.twitch.tv"              # This is Twitchs IRC server
 PORT = 6667                         # Twitchs IRC server listens on port 6767
-NICK = "protosem"            # Twitch username your using for your bot
-PASS = "oauth:esbncd6od23uks9zpv1loetusuk01b" # your Twitch OAuth token
-CHAN = "#protosem"                   # the channel you want the bot to join.
+NICK = "YourUsername"            # Twitch username your using for your bot
+PASS = "oauth:YourToken" # your Twitch OAuth token
+CHAN = "#YourChannel"                   # the channel you want the bot to join.
 RATE = (20/30) # messages per seccond
 BAN_PAT = [
     r"swear",
     r"fuck",
-    r"top tup"
 ]
--- Twitter_bot-Rpi/SillyTweeter/SillyTweeter.py
+++ Twitter_bot-Rpi/SillyTweeter/SillyTweeter.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 import sys
 from twython import Twython
-CONSUMER_KEY = 'ZEl5LkbNnjXGdY9MEcP5bIVVY'
-CONSUMER_SECRET = 'u63gTWJ7vL5AHpR5dSW3GHU6fYGLw8wz5jY1wWoSx68qCTmSOS'
-ACCESS_KEY = '2327212614-9uqCRgSlStR6SpYbtEnyeUT7pEoEHFNCMtQfh7i'
-ACCESS_SECRET = 'D5VZyjdhsID1qKJsCLNb5imXiZdItezgrOgH7fn1jr7Jt'
+CONSUMER_KEY = 'YourConsumerKey'
+CONSUMER_SECRET = 'YourConsumerSecret'
+ACCESS_KEY = 'YourAccesKey'
+ACCESS_SECRET = 'YourAccessSecret'
 
 api = Twython(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_KEY,ACCESS_SECRET) 
 
